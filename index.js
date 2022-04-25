230d21cf097c9e2facd6f8ceb2a8d5856dac02af
jesse zenon <jesse.zenon@gmail.com>
2022-04-25 15:23:36 -0500

    commit tree 853cae719efc804671545153f582b5a315c2fc78

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function selectingDrivers() {
+  returnFirstTwoDrivers();
+  returnLastTwoDrivers();
+}
+
+const returnFirstTwoDrivers = function (arrayOfDrivers) {
+  return arrayOfDrivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (arrayOfDrivers) {
+  return arrayOfDrivers.splice(-2);
+};
+
+function createFareMultiplier(num) {
+  return function (fare) {
+    return num * fare;
+  };
+}
+
+const fareDoubler = (fare) => {
+  createFareMultiplier();
+};
+
+const fareTripler = (fare) => {
+  createFareMultiplier();
+};
